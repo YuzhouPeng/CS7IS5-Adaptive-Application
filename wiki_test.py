8c8837d92798d630b2346d12e908255bde197855
ShrubinS <shrubin93.skn@gmail.com>
2018-03-23 16:38:45 +0000

    getting links with index and other required values

--- wiki_test.py
+++ wiki_test.py
@@ -7,6 +7,7 @@ Created on Wed Mar  7 17:16:57 2018
 
 import wikipedia
 import io
+import bs4 as bs
 
 #wikipedia.summary("art")
 test = wikipedia.page("Music")
@@ -15,6 +16,39 @@ test = wikipedia.page("Music")
 #test.url
 #test.content
 tlist = test.links
+tcontent = test.content
+tpage = test.html()
+
+soup = bs.BeautifulSoup(tpage, "html5lib")
+
+
+links = soup.findAll('a')        
+link_strings = []
+for link in links:
+    if link.string == "study of the history of music":
+        print(link)
+        print(link['title'])
+    if link.string == "aesthetic examination of music":
+        print(link)
+        
+        
+links = soup.findAll('a')        
+link_dict = {}
+for link in links:
+    try:
+        k = tcontent.lower().find(link.string.lower())
+        #if link.string == "study of the history of music":
+        val = link['href'].replace("/wiki/", "").replace("(", "").replace(")", "") + "   " + str(k) 
+        link_dict[link['title']] = val
+    except ValueError:
+        pass
+    except TypeError:
+        pass
+    except KeyError:
+        pass
+    except AttributeError:
+        pass
+        
 
 #tlist = [str(s).translate(None,"()") for s in tlist]    
 tlist = [s.replace('(', '') for s in tlist]
@@ -25,3 +59,8 @@ with io.open("test_file.txt", "w", encoding="utf-8") as f:
     for item in tlist:
         f.write("%s\n" % item)
 f.close()
+
+with io.open("output.txt", "w", encoding="utf-8") as k:
+    for key, value in link_dict.items():
+        k.write([key, val])
+k.close()
