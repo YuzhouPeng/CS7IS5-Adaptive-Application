2a379acfce07a01a1fd076f9e31a93cd5631cc52
ShrubinS <shrubin93.skn@gmail.com>
2018-03-23 17:37:28 +0000

    added comments, removed old code

--- wiki_test.py
+++ wiki_test.py
@@ -1,43 +1,30 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  7 17:16:57 2018
-
-@author: Vimanyu
-"""
-
 import wikipedia
-import io
 import bs4 as bs
 
-#wikipedia.summary("art")
+"""Get the article details such as Name, link, index of link on page, summary
+   To store in DB"""
+
+# wikipedia.summary("art") -- get summary
 test = wikipedia.page("Music")
 
-#test.title
-#test.url
-#test.content
 tlist = test.links
 tcontent = test.content
 tpage = test.html()
 
 soup = bs.BeautifulSoup(tpage, "html5lib")
-
-
-links = soup.findAll('a')        
-link_strings = []
-for link in links:
-    if link.string == "study of the history of music":
-        print(link)
-        print(link['title'])
-    if link.string == "aesthetic examination of music":
-        print(link)
         
+# tlist = [str(s).translate(None,"()") for s in tlist]
+tlist = [s.replace('(', '') for s in tlist]
+tlist = [s.replace(')', '') for s in tlist]
+tlist = [s.replace(' ', '_') for s in tlist]
         
 links = soup.findAll('a')        
 link_dict = {}
 for link in links:
     try:
         k = tcontent.lower().find(link.string.lower())
-        #if link.string == "study of the history of music":
+        # if link.string == "study of the history of music":
         val = link['href'].replace("/wiki/", "").replace("(", "").replace(")", "") + "   " + str(k) 
         link_dict[link['title']] = val
     except ValueError:
@@ -48,19 +35,12 @@ for link in links:
         pass
     except AttributeError:
         pass
-        
 
-#tlist = [str(s).translate(None,"()") for s in tlist]    
-tlist = [s.replace('(', '') for s in tlist]
-tlist = [s.replace(')', '') for s in tlist]
-tlist = [s.replace(' ', '_') for s in tlist]
-
-with io.open("test_file.txt", "w", encoding="utf-8") as f:
-    for item in tlist:
-        f.write("%s\n" % item)
-f.close()
-
-with io.open("output.txt", "w", encoding="utf-8") as k:
-    for key, value in link_dict.items():
-        k.write([key, val])
-k.close()
+link_dict_final = {}
+for item in tlist:
+    try:
+        link_dict_final[item] = link_dict[item]
+    except KeyError:
+        pass
+    
+# Next, write to database
