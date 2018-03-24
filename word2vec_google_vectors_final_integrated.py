3191601f4a2e38fc06305970f8a36ccf3c6034c2
Samim Ekram <samim.ekram@gmail.com>
2018-03-24 15:41:18 +0000

    Add files via upload

--- word2vec_google_vectors_final_integrated.py
+++ word2vec_google_vectors_final_integrated.py
@@ -1,8 +1,9 @@
 import gensim
 model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
-print model.similarity('woman', 'man') 
+#print model.similarity('woman', 'man') 
 import wikipedia
 import bs4 as bs
+import numpy as np
 
 """Get the article details such as Name, link, index of link on page, summary
    To store in DB"""
@@ -28,7 +29,7 @@ for link in links:
         k = tcontent.lower().find(link.string.lower())
         # if link.string == "study of the history of music":
         val = link['href'].replace("/wiki/", "").replace("(", "").replace(")", "") + "   " + str(k) 
-        link_dict[link['title']] = val
+        link_dict[link['title']] = k
     except ValueError:
         pass
     except TypeError:
@@ -45,11 +46,11 @@ for item in tlist:
     except KeyError:
         pass
 
-list = []
+listt = []
 new_wordlist=[]
 for i in link_dict_final:
     try:
-        list.append(model.similarity('song', i))
+        listt.append(model.similarity('song', i))
         #print model.similarity('song', i)
         #print i
         new_wordlist.append(i)
@@ -59,7 +60,7 @@ for i in link_dict_final:
 
         
 #print list
-len(list)
+len(listt)
 len(new_wordlist)
 final_word2vec = {k: link_dict_final[k] for k in link_dict_final.viewkeys() & set(new_wordlist)} #to make the list of the word that are comparable in the page with their indexes  
 len(final_word2vec)
@@ -71,6 +72,17 @@ for i in final_word2vec:
         #print i
     except Exception:
         pass
+        
+list1=list(final_word2vec.keys())
+list2=list(final_word2vec.values())
+list3=list(finalvalues_list)
 
+rows = zip(list1,list2,list3)
+import csv
+
+with open("data.csv", "w") as f:
+    writer = csv.writer(f)
+    for row in rows:
+        writer.writerow(row)
 #just to clarify finally we have 3 list 1st final_word2vec with has the elements title and keys and finalvalues_list which has all the values
 #and use final_word2vec.keys() for the word , final_word2vec.values() for the indexes and finalvalues_list to get the similarity values
