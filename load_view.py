9fdafcb29036b844d27f527e11c1348282875851
ShrubinS <shrubin93.skn@gmail.com>
2018-03-28 19:27:18 +0100

    test file load view updated

--- load_view.py
+++ load_view.py
@@ -2,7 +2,7 @@ import pickle
 
 
 if __name__=="__main__":
-    with open('/Users/apple/projects/CS7IS-adaptive-applications/CS7IS5-Adaptive-Application/output-sport/Football.p', 'rb') as fp:
+    with open('/Users/apple/projects/CS7IS-adaptive-applications/CS7IS5-Adaptive-Application/output/Music.p', 'rb') as fp:
         data = pickle.load(fp)
 
     for key, value in data.items():
@@ -15,3 +15,13 @@ if __name__=="__main__":
 
     print(len(d.keys()))
     print(data['page'])
+
+  
+for key, value in data['data'].items():
+    start_index = value['index_start']
+    end_index = value['index_end']
+    content = data['content']
+
+    print ("Actual: ", content[start_index:end_index], "\n",
+           "Expected: ", key)
+    
